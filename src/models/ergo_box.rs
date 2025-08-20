0440d1ce88578795e8c3203fc15837ec3176dd56
arobsn <87387688+arobsn@users.noreply.github.com>
2025-08-19 22:03:15 -0300

    feat: add serde serialization

--- src/models/ergo_box.rs
+++ src/models/ergo_box.rs
@@ -1,28 +1,46 @@
+use serde::{Deserialize, Serialize};
+
 use crate::models::HashDigest;
 
+#[derive(Deserialize, Serialize)]
 pub struct ErgoBox {
+    #[serde(rename = "boxId")]
     pub id: HashDigest,
 
+    #[serde(rename = "ergoTree")]
     pub ergo_tree: Vec<u8>,
+    #[serde(rename = "creationHeight")]
     pub creation_height: u32,
     pub value: u64,
+    #[serde(rename = "assets")]
     pub tokens: Vec<Token>,
+    #[serde(rename = "additionalRegisters")]
     pub registers: NonMandatoryRegisters,
 
     pub index: u16,
+    #[serde(rename = "transactionId")]
     pub transaction_id: HashDigest,
 }
 
+#[derive(Deserialize, Serialize)]
 pub struct Token {
+    #[serde(rename = "tokenId")]
     pub id: HashDigest,
     pub amount: u64,
 }
 
+#[derive(Deserialize, Serialize)]
 pub struct NonMandatoryRegisters {
+    #[serde(rename = "R4")]
     pub r4: Option<Vec<u8>>,
+    #[serde(rename = "R5")]
     pub r5: Option<Vec<u8>>,
+    #[serde(rename = "R6")]
     pub r6: Option<Vec<u8>>,
+    #[serde(rename = "R7")]
     pub r7: Option<Vec<u8>>,
+    #[serde(rename = "R8")]
     pub r8: Option<Vec<u8>>,
+    #[serde(rename = "R9")]
     pub r9: Option<Vec<u8>>,
 }
--- src/models/ergo_transaction.rs
+++ src/models/ergo_transaction.rs
@@ -1,8 +1,12 @@
+use serde::{Deserialize, Serialize};
+
 use crate::models::{HashDigest, ergo_box::ErgoBox};
 
+#[derive(Deserialize, Serialize)]
 pub struct ErgoTransaction {
     pub id: HashDigest,
     pub inputs: Vec<ErgoBox>,
     pub outputs: Vec<ErgoBox>,
+    #[serde(rename = "inclusionHeight")]
     pub height: u32,
 }
