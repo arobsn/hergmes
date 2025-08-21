30e949bf5c1bb93ef63977b9c0cd1af0347c7ffe
arobsn <87387688+arobsn@users.noreply.github.com>
2025-08-20 22:14:26 -0300

    feat: add .env support

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
+use std::error::Error;
+
+use dotenvy::dotenv;
 use hergmes::telemetry::{self, default_subscriber};
 use tracing::info;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
+    dotenv()?;
     telemetry::init(default_subscriber());
 
     info!("Hello, world!");
+    Ok(())
 }
